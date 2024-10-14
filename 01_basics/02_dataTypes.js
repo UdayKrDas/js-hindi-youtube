02915d956d626c990c348d78157ace37a60ac336
Uday Kumar Das <contactudayhere@gmail.com>
2024-10-14 18:13:12 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+let name = "Ricky"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint =>for larger numbers than that
+// string => can use "" or '', Hitesh preferred ""
+// boolean => true/false
+// null => standalone value 
+// undefined => no value assigned
+// symbol => unique
+
+
+// object => to be discussed later on further tutorials
+
+console.log(typeof age)
+console.log(typeof "Ricky")
+console.log(typeof undefined)
+console.log(typeof null)
