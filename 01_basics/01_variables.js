74242806a7d8e995fd74870b13bb2e246cd87a7a
Uday Kumar Das <contactudayhere@gmail.com>
2024-10-14 17:22:30 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail =  "ricky@google.com"
+var accountPassword = "12345"
+accountCity = "North Lakhimpur"
+let accountState;
+
+// account Id = 2 not allowed
+
+accountEmail = "hchc@google.com"
+accountPassword = "54321"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
