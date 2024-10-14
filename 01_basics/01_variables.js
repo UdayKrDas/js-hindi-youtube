d9a2cb39494da0b61eef89ee99c92f49e99e134f
Uday Kumar Das <contactudayhere@gmail.com>
2024-10-14 17:33:38 +0000

    push

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,5 +10,10 @@ accountEmail = "hchc@google.com"
 accountPassword = "54321"
 accountCity = "Bengaluru"
 
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 console.log(accountId)
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
