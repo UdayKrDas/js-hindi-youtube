f1ebcab9e704bc4e398f9cbedd8ac2236c25b14b
Uday Kumar Das <contactudayhere@gmail.com>
2024-10-23 14:25:00 +0000

    03_conversionOperation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,24 @@
+
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// ****************Operatons******************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Ricky "
+let str2 = "Drax"
+
+let str3 = str1 + str2
+console.log(str3);
