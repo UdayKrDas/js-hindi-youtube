a0391ff43295c3a367ce51c525013e01b644a082
Uday Kumar Das <contactudayhere@gmail.com>
2024-10-26 18:16:07 +0000

    strings 2

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,2 +1,16 @@
 const name = "Ricky"
 const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String ('hiteshhc')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
